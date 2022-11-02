9f450f6065456509bb2cdd650cde51b058892822
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-11-02 12:54:00 +0100

    script

--- Prácticas/XML/Program/main.py
+++ Prácticas/XML/Program/main.py
@@ -1,30 +1,140 @@
-from lxml import etree
-# import module
+# from lxml import etree
+# # import module
 
-def xml_html_converter():
+# def xml_html_converter():
 
-    xslt_doc = etree.parse("persona.xslt")
-    xslt_transformer = etree.XSLT(xslt_doc)
+#     xslt_doc = etree.parse("persona.xslt")
+#     xslt_transformer = etree.XSLT(xslt_doc)
 
-    #El xml inicial
-    source_doc = etree.parse("xml.xml")
-    #El html final
-    output_doc = xslt_transformer(source_doc)
-    return output_doc
+#     #El xml inicial
+#     source_doc = etree.parse("xml.xml")
+#     #El html final
+#     output_doc = xslt_transformer(source_doc)
+#     return output_doc
 
 
-def writeToArchive(fileReaded, fileWritten):
-    # open both files
-    with open(fileWritten, 'w') as secondfile:
-        # read content from first file
-        for line in fileReaded:
-            # append content to second file
-            secondfile.write(line)
-    print("SUCCESS")
+# def writeToArchive(fileReaded, fileWritten):
+#     # open both files
+#     with open(fileWritten, 'w') as secondfile:
+#         # read content from first file
+#         for line in fileReaded:
+#             # append content to second file
+#             secondfile.write(line)
+#     print("SUCCESS")
 
-fileReaded = str(xml_html_converter())
-print(fileReaded)
-fileWritten = "persona.html"
+# fileReaded = str(xml_html_converter())
+# print(fileReaded)
+# fileWritten = "redSocial.html"
 
-fileRTrimmed = fileReaded.split("\n")
-writeToArchive(fileRTrimmed,fileWritten)
+# fileRTrimmed = fileReaded.split("\n")
+# writeToArchive(fileRTrimmed,fileWritten)
+
+#-------------------------------
+import re
+def leerXML(fileXML):
+    html = open("redSocial.html", "a")
+    fileXMLOpened = open(fileXML)
+    lines = fileXMLOpened.readlines()
+    inicializarHTML()
+    try:
+        for linea in lines:
+            escribirLinea(linea)
+        html.write("</body>")
+    finally:
+        fileXMLOpened.close()
+        print("SUCCESS")
+
+def escribirLineaAHTML(line):
+    html = open("redSocial.html", "a")
+    try:
+        html.write(line)
+    finally:
+        html.close()
+
+def inicializarHTML():
+    html = open("redSocial.html", "w")
+
+    html.write("<!DOCTYPE HTML>\n")
+    html.write("<html lang=\"es\">\n")
+    html.write("<head>\n")
+    html.write("\t<!-- Datos que describen el documento --> \n")
+    html.write("\t<meta charset=\"UTF-8\" />\n")
+    html.write("\t<!--Metadatos de los documentos HTML5-->\n")
+    html.write("\t<meta name =\"author\" content =\"Eduardo Blanco Bielsa\" />\n")
+    html.write("\t<meta name =\"description\" content =\"Página principal\" />\n")
+    html.write("\t<meta name =\"keywords\" content =\"Personas\" />\n")
+    html.write("\t<!--Definición de la ventana gráfica-->\n")
+    html.write("\t<meta name =\"viewport\" content =\"width=device-width, initial-scale=1.0\" />\n")
+    html.write("\t<title>RedSocial</title>\n")
+    html.write("\t<!--<link rel=\"stylesheet\" type=\"text/css\" href=\"estilo/estilo.css\" />-->\n" + "</head>\n")
+    html.write("\n<body>\n")
+    html.write("\t<h1>Red Social</h1>\n")
+def escribirLinea(linea):
+    if("nombre" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<nombre>", "", linea)
+        linea = re.sub("</nombre>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t<h2>\n\t\t\tNombre: " + linea)
+    elif("apellidos" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<apellidos>", "", linea)
+        linea = re.sub("</apellidos>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tApellidos: " + linea)
+    elif ("fecha" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<fecha>", "", linea)
+        linea = re.sub("</fecha>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tFecha de nacimiento: " + linea)
+    elif ("lugar_nacimiento" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<lugar_nacimiento>", "", linea)
+        linea = re.sub("</lugar_nacimiento>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tLugar de nacimiento: " + linea)
+    elif ("coordenadas_nacimiento" in linea):
+
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<coordenadas_nacimiento>", "", linea)
+        linea = re.sub("</coordenadas_nacimiento>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tCoordenadas de nacimiento: " + linea)
+    elif ("lugar_residencia" in linea):
+
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<lugar_residencia>", "", linea)
+        linea = re.sub("</lugar_residencia>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tLugar de residencia: " + linea)
+    elif ("coordenadas_residencia" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<coordenadas_residencia>", "", linea)
+        linea = re.sub("</coordenadas_residencia>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tCoordenadas de residencia: " + linea)
+    elif ("fotografia" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<fotografia>", "", linea)
+        linea = re.sub("</fotografia>", "", linea)#con el CDATA
+        linea = re.sub("<!\[CDATA\[", "", linea)
+        linea = re.sub("]]>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tFotografia: " + linea)
+    elif ("video" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<video>", "", linea)
+        linea = re.sub("</video>", "", linea)
+        linea = re.sub("<!\[CDATA\[", "", linea)
+        linea = re.sub("]]>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tVideo: " + linea)
+    elif ("comentarios" in linea):
+        linea = re.sub("\t", "", linea)
+        linea = re.sub("<comentarios>", "", linea)
+        linea = re.sub("</comentarios>", "", linea)
+        linea.rstrip()
+        escribirLineaAHTML("\t\t\tComentarios: " + linea + "\t\t</h2>\n")
+
+leerXML("xml.xml")
--- Prácticas/XML/Program/persona.xslt
+++ Prácticas/XML/Program/persona.xslt
@@ -1,23 +1,23 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
 
-<xsl:template match="persona">
+<xsl:template match="redSocial">
 	<html>
 		<body>
 			<h1>Social Network</h1>
 				<!-- Nivel 1 -->
-				<xsl:value-of select="persona">
-					<p><xsl:value-of select="nombre" /></p>
-					<p><xsl:value-of select="apellidos" /></p>
-					<p><xsl:value-of select="fecha" /></p>
-					<p><xsl:value-of select="lugar_nacimiento" /></p>
-					<p><xsl:value-of select="coordenadas_nacimiento" /></p>
-					<p><xsl:value-of select="lugar_residencia" /></p>
-					<p><xsl:value-of select="coordenadas_residencia" /></p>
+				<xsl:foreach select="redSocial/persona">
+					<p>Nombre: <xsl:value-of select="nombre" /></p>
+					<p>Apellidos: <xsl:value-of select="apellidos" /></p>
+					<p>Fecha: <xsl:value-of select="fecha" /></p>
+					<p>Lugar de nacimiento: <xsl:value-of select="lugar_nacimiento" /></p>
+					<p>Coordenadas de nacimiento: <xsl:value-of select="coordenadas_nacimiento" /></p>
+					<p>Lugar de residencia: <xsl:value-of select="lugar_residencia" /></p>
+					<p>Coordenadas de residencia: <xsl:value-of select="coordenadas_residencia" /></p>
 					<p><xsl:value-of select="fotografia" /></p>
 					<p><xsl:value-of select="video" /></p>
-					<p><xsl:value-of select="comentarios" /></p>
-				</xsl:value-of> 
+					<p>Comentarios: <xsl:value-of select="comentarios" /></p>
+				</xsl:foreach> 
 		</body>
 	</html>
 </xsl:template>
