cf5cbdf8ea43e1ab8fe20552e365c79b1d739a65
Eduardo Blanco Bielsa <blanc@MacBook-Pro-de-Eduardo.local>
2022-11-02 18:32:46 +0100

    script conversor xml-html finalizado

--- Prácticas/XML/Program/main.py
+++ Prácticas/XML/Program/main.py
@@ -1,35 +1,3 @@
-# from lxml import etree
-# # import module
-
-# def xml_html_converter():
-
-#     xslt_doc = etree.parse("persona.xslt")
-#     xslt_transformer = etree.XSLT(xslt_doc)
-
-#     #El xml inicial
-#     source_doc = etree.parse("xml.xml")
-#     #El html final
-#     output_doc = xslt_transformer(source_doc)
-#     return output_doc
-
-
-# def writeToArchive(fileReaded, fileWritten):
-#     # open both files
-#     with open(fileWritten, 'w') as secondfile:
-#         # read content from first file
-#         for line in fileReaded:
-#             # append content to second file
-#             secondfile.write(line)
-#     print("SUCCESS")
-
-# fileReaded = str(xml_html_converter())
-# print(fileReaded)
-# fileWritten = "redSocial.html"
-
-# fileRTrimmed = fileReaded.split("\n")
-# writeToArchive(fileRTrimmed,fileWritten)
-
-#-------------------------------
 import re
 def leerXML(fileXML):
     html = open("redSocial.html", "a")
@@ -37,8 +5,11 @@ def leerXML(fileXML):
     lines = fileXMLOpened.readlines()
     inicializarHTML()
     try:
+        cont = 1
         for linea in lines:
-            escribirLinea(linea)
+            escribirLinea(linea, cont)
+            if("nombre" in linea):
+                cont+=1
         html.write("</body>")
     finally:
         fileXMLOpened.close()
@@ -66,75 +37,76 @@ def inicializarHTML():
     html.write("\t<!--Definición de la ventana gráfica-->\n")
     html.write("\t<meta name =\"viewport\" content =\"width=device-width, initial-scale=1.0\" />\n")
     html.write("\t<title>RedSocial</title>\n")
-    html.write("\t<!--<link rel=\"stylesheet\" type=\"text/css\" href=\"estilo/estilo.css\" />-->\n" + "</head>\n")
+    html.write("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"layout.css\" />\n")
+    html.write("\t<link rel=\"stylesheet\" type=\"text/css\" href=\"estilo.css\" />\n" + "</head>\n")
     html.write("\n<body>\n")
     html.write("\t<h1>Red Social</h1>\n")
-def escribirLinea(linea):
+def escribirLinea(linea, cont):
     if("nombre" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<nombre>", "", linea)
         linea = re.sub("</nombre>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t<h2>\n\t\t\tNombre: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t<h3>Persona" + cont.__str__() + "</h3>\n\t\t\t<h4>\n\t\t\t\t<p>Nombre: " + enlace[0] + " </p>\n")
     elif("apellidos" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<apellidos>", "", linea)
         linea = re.sub("</apellidos>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tApellidos: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Apellidos: " + enlace[0] + " </p>\n")
     elif ("fecha" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<fecha>", "", linea)
         linea = re.sub("</fecha>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tFecha de nacimiento: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Fecha de nacimiento: " + enlace[0] + " </p>\n")
     elif ("lugar_nacimiento" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<lugar_nacimiento>", "", linea)
         linea = re.sub("</lugar_nacimiento>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tLugar de nacimiento: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Lugar de nacimiento: " + enlace[0] + " </p>\n")
     elif ("coordenadas_nacimiento" in linea):
 
         linea = re.sub("\t", "", linea)
         linea = re.sub("<coordenadas_nacimiento>", "", linea)
         linea = re.sub("</coordenadas_nacimiento>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tCoordenadas de nacimiento: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Coordenadas de nacimiento: " + enlace[0] + " </p>\n")
     elif ("lugar_residencia" in linea):
 
         linea = re.sub("\t", "", linea)
         linea = re.sub("<lugar_residencia>", "", linea)
         linea = re.sub("</lugar_residencia>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tLugar de residencia: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Lugar de residencia: " + enlace[0] + " </p>\n")
     elif ("coordenadas_residencia" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<coordenadas_residencia>", "", linea)
         linea = re.sub("</coordenadas_residencia>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tCoordenadas de residencia: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Coordenadas de residencia: " + enlace[0] + " </p>\n")
     elif ("fotografia" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<fotografia>", "", linea)
         linea = re.sub("</fotografia>", "", linea)#con el CDATA
         linea = re.sub("<!\[CDATA\[", "", linea)
         linea = re.sub("]]>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tFotografia: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Fotografia: <a href="+enlace[0]+">foto</a>\n")
     elif ("video" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<video>", "", linea)
         linea = re.sub("</video>", "", linea)
         linea = re.sub("<!\[CDATA\[", "", linea)
         linea = re.sub("]]>", "", linea)
-        linea.rstrip()
-        escribirLineaAHTML("\t\t\tVideo: " + linea)
+        enlace = linea.split("\n")
+        escribirLineaAHTML("\t\t\t\t<p>Video: <a href="+enlace[0]+">video</a>\n")
     elif ("comentarios" in linea):
         linea = re.sub("\t", "", linea)
         linea = re.sub("<comentarios>", "", linea)
         linea = re.sub("</comentarios>", "", linea)
         linea.rstrip()
-        escribirLineaAHTML("\t\t\tComentarios: " + linea + "\t\t</h2>\n")
+        escribirLineaAHTML("\t\t\t\t<p>Comentarios: " + linea + "\t\t\t</h4>\n")
 
 leerXML("xml.xml")
