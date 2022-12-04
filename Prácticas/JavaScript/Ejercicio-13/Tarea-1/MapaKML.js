2c17354f0f94decd82e2a6b7646724bbdded720d
gitblanc <UO285176@uniovi.es>
2022-12-04 14:24:25 +0100

    p14 terminada

--- Prácticas/JavaScript/Ejercicio-13/Tarea-1/MapaKML.js
+++ Prácticas/JavaScript/Ejercicio-13/Tarea-1/MapaKML.js
@@ -1,27 +1,104 @@
-class MapaKML{
+//tomado del ejemplo de cueva
+class MapaKML {
     constructor(){
-        this.src = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
+        this.resultKml;
+        this.numFiles = 0;
+        this.totalBytes = 0;
+        this.files;
+        this.src = '/Tarea-1/redSocial.kml';
+        navigator.geolocation.getCurrentPosition(this.cargarDatos.bind(this), this.verErrores.bind(this));
     }
-    //https://developers.google.com/maps/documentation/javascript/kml
-    initMap() {
-        var map;
-        map = new google.maps.Map(document.querySelector('main'), {
-            center: new google.maps.LatLng(43.3672702, -5.8502461),
-            zoom: 10,
-            mapTypeId: 'terrain'
-        });
-        
-        var kmlLayer = new google.maps.KmlLayer(this.src, {
-            suppressInfoWindows: true,
-            preserveViewport: false,
-            map: map
-        });
-        kmlLayer.addListener('click', function(event) {
-            var content = event.featureData.infoWindowHtml;
-            var testimonial = document.querySelector('section');
-            testimonial.innerHTML = content;
-        });
+    verErrores(error){
+        switch(error.code){
+            case error.PERMISSION_DENIED:
+                this.mensaje = "El usuario no permite la petición de geolocalización"
+                break;
+            case error.POSITION_UNAVAILABLE:
+                this.mensaje = "Información de geolocalización no disponible"
+                break;
+            case error.TIMEOUT:
+                this.mensaje = "La petición de geolocalización ha caducado"
+                break;
+            case error.UNKNOWN_ERROR:
+                this.mensaje = "Se ha producido un error desconocido"
+                break;
+        }
     }
+    cargarDatos(c){
+        this.longitud = c.coords.longitude;
+        this.latitud = c.coords.latitude;
+    }
+    mostrarMapa(){
+        $('h2').after("<script src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyApmnMoES2D1Bm3GmduR8lEhRXYUk_hh78&callback=mapa.initMap&v=weekly\"defer></script>")
+    }
+    initMap(){
+        var lugar = {lat: this.latitud, lng: this.longitud};
+        var mapa = new google.maps.Map(document.querySelector('main'),{center:lugar, zoom: 8});
+        //this.parsearKml(mapa);
+        // var kmlLayer = new google.maps.KmlLayer(this.src, {
+        //     suppressInfoWindows: true,
+        //     preserveViewport: false,
+        //     map: mapa
+        // });
+        // kmlLayer.addListener('click', function(event) {
+        //     var content = event.featureData.infoWindowHtml;
+        //     var testimonial = document.querySelector('section');
+        //     testimonial.innerHTML = content;
+        // });
+    }
+    leerKml(file){
+        var nombre = "<strong>"+file.name + "</strong>";
+        var lector;
+        var tipoKml = "kml";
+      
+        $('label').before("<section>"+nombre);
+
+        //comprobampos el tipo de los ficheros   
+        if(file.name.split(".")[1] === (tipoKml)){
+            $("h2:last").after("<p name=\"" +  file.name + "\"></p></section>");
+
+            lector = new FileReader();
+
+            lector.onload = function(evento){ 
+                this.resultKml = lector.result;
+            }
+            lector.readAsText(file);
+        }
+        else{
+            nombre = "<p>El tipo de l archivo no está contemplado...</p></section>";
+            $('h3:last').before(nombre);
+        }
+    }
+    mostrarArchivos(){
+        var file;
+     
+        $('label').before("<h3>Contenido de los archivos:</h3>");
+        for (var i = 0; i <  this.numFiles; i++) {
+            file = this.fileArray[i];
+            this.leerKml(file);
+        }
+    }
+    mostrarInfoArchivos(){
+        this.totalBytes = 0;
+        this.fileArray  = document.querySelector("input").files;
+        this.numFiles = this.fileArray.length;
+
+        var titulo = "<h3>Información de los archivos:</h3>"
+        for (var i = 0; i <  this.numFiles; i++) {
+            this.totalBytes += this.fileArray[i].size;
+        }
+        var informacion="";
+        for (var i = 0; i <  this.numFiles; i++) {
+            informacion += "<p>Archivo " + (i+1) +" = "+ this.fileArray[i].name  + ", Tamaño: " + this.fileArray[i].size +" bytes " + ", Tipo: " + this.fileArray[i].type+"</p>" ;
+        }
+        //info de los archivos
+        $('label').before("" + titulo +" <p>Total de bytes: " + this.totalBytes + "<p>" + informacion);
+       
+        this.mostrarArchivos();
+    }
+    // parsearKml(map){
+
+    // }
 }
 
-var mapaKML = new MapaKML();
+var mapa = new MapaKML();
---
+++ Prácticas/JavaScript/Ejercicio-14/Ejercicio14.js
@@ -0,0 +1,125 @@
+//API History
+class Historial{
+    constructor(){
+
+    }
+    goBack(){
+         window.history.back();
+    }
+    goForward(){
+        window.history.forward();
+    }
+}
+//API Canvas -> https://www.javascripttutorial.net/web-apis/javascript-canvas/
+class Pacman{
+    constructor(){
+    }
+
+    // A utility function to draw a rectangle with rounded corners.
+      
+    roundedRect(ctx, x, y, width, height, radius) {
+        ctx.strokeStyle = "#0026ff";
+        ctx.beginPath();
+        ctx.moveTo(x, y + radius);
+        ctx.arcTo(x, y + height, x + radius, y + height, radius);
+        ctx.arcTo(x + width, y + height, x + width, y + height - radius, radius);
+        ctx.arcTo(x + width, y, x + width - radius, y, radius);
+        ctx.arcTo(x, y, x, y + radius, radius);
+        ctx.stroke();
+    }
+
+    createPacman() {
+        const canvas = document.getElementById('canvas');
+        if (canvas.getContext) {
+          const ctx = canvas.getContext('2d');
+          
+          
+          this.roundedRect(ctx, 12, 12, 200, 200, 15);
+          this.roundedRect(ctx, 19, 19, 186, 186, 9);
+          this.roundedRect(ctx, 53, 53, 49, 33, 10);
+          this.roundedRect(ctx, 53, 119, 49, 16, 6);
+          this.roundedRect(ctx, 135, 53, 49, 33, 10);
+          this.roundedRect(ctx, 135, 119, 25, 49, 10);
+            
+          ctx.fillStyle = 'yellow';
+          ctx.beginPath();
+          ctx.arc(37, 37, 13, Math.PI / 7, -Math.PI / 7, false);
+          ctx.lineTo(31, 37);
+          ctx.fill();
+      
+          ctx.fillStyle = 'orange';
+          for (let i = 0; i < 10; i++) {
+            ctx.fillRect(51 + i * 16, 35, 4, 4);
+          }
+      
+          for (let i = 0; i < 10; i++) {
+            ctx.fillRect(115, 51 + i * 16, 4, 4);
+          }
+          
+          for (let i = 0; i < 10; i++) {
+            ctx.fillRect(51 + i * 16, 99, 4, 4);
+          }
+          
+          ctx.fillStyle = 'red';
+          ctx.beginPath();
+          ctx.moveTo(83, 116);
+          ctx.lineTo(83, 102);
+          ctx.bezierCurveTo(83, 94, 89, 88, 97, 88);
+          ctx.bezierCurveTo(105, 88, 111, 94, 111, 102);
+          ctx.lineTo(111, 116);
+          ctx.lineTo(106.333, 111.333);
+          ctx.lineTo(101.666, 116);
+          ctx.lineTo(97, 111.333);
+          ctx.lineTo(92.333, 116);
+          ctx.lineTo(87.666, 111.333);
+          ctx.lineTo(83, 116);
+          ctx.fill();
+      
+          ctx.fillStyle = 'white';
+          ctx.beginPath();
+          ctx.moveTo(91, 96);
+          ctx.bezierCurveTo(88, 96, 87, 99, 87, 101);
+          ctx.bezierCurveTo(87, 103, 88, 106, 91, 106);
+          ctx.bezierCurveTo(94, 106, 95, 103, 95, 101);
+          ctx.bezierCurveTo(95, 99, 94, 96, 91, 96);
+          ctx.moveTo(103, 96);
+          ctx.bezierCurveTo(100, 96, 99, 99, 99, 101);
+          ctx.bezierCurveTo(99, 103, 100, 106, 103, 106);
+          ctx.bezierCurveTo(106, 106, 107, 103, 107, 101);
+          ctx.bezierCurveTo(107, 99, 106, 96, 103, 96);
+          ctx.fill();
+      
+          ctx.fillStyle = 'black';
+          ctx.beginPath();
+          ctx.arc(101, 102, 2, 0, Math.PI * 2, true);
+          ctx.fill();
+          
+          ctx.beginPath();
+          ctx.arc(89, 102, 2, 0, Math.PI * 2, true);
+          ctx.fill();
+        }
+    }
+}
+
+class FullScreen{
+    constructor(){
+
+    }
+    pantallaCompleta(){
+        /* Get the element you want to display in fullscreen */
+        var elem = document.querySelector("#canvas");
+
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
+    }
+}
+
+var historial = new Historial();
+var pac = new Pacman();
+var pantalla = new FullScreen();
+
